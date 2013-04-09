4dd738eb257c728def956436c20bc240489f633e
James Hwang <james.hwang.usc@gmail.com>
2013-04-08 23:45:54 -0700

    Added crew requirements

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -1,7 +1,7 @@
 require_relative('../plane')
 
 describe Plane do
-	subject(:plane_787) { Plane.new }
+	subject(:plane_787) { Plane.new("787", { pilot: 1, co_pilot: 1 }) }
 
 	describe "initalized state" do
 	  it { should_not be_nil }
@@ -42,7 +42,62 @@ describe Plane do
 		  end
 		end
 		it "returns speed"
+	end
 
+	describe "crew info" do
+		before(:each) do
+			@tom = double("Crew", name: "Tom", role: "Pilot")
+			@jane = double("Crew", name: "Jane", role: "Co-Pilot")
+			@tom.stub(:to_h).and_return({ name: @tom.name, role: @tom.role })
+			@jane.stub(:to_h).and_return({ name: @jane.name, role: @jane.role })
+			@tom.stub(:role).and_return(@tom.role)
+			@jane.stub(:role).and_return(@jane.role)
+		end
+
+		it "adds crew members to itself" do
+			expect(plane_787.add_crew(@tom)).to eq [{name: "Tom", role: "Pilot"}]
+		end
+		context "after adding the Pilot and Co-Pilot" do
+			before(:each) do
+				plane_787.add_crew @tom
+				plane_787.add_crew @jane
+			end
+			it "#crew should return a list of people with their roles" do
+				expect(plane_787.crew).to eq [{name: "Tom", role: "Pilot"},
+																			{name: "Jane", role: "Co-Pilot"}]
+			end
+
+			it "#role_call returns a list of crew roles and number of members per role" do
+				expect(plane_787.role_call).to eq({pilot: 1, co_pilot: 1})
+			end
+
+			it "#role_call returns 1 Pilot and 2 Co-Pilots" do
+				plane_787.add_crew @jane
+				expect(plane_787.role_call).to eq({pilot: 1, co_pilot: 2})
+			end
+
+			it "#roles returns only roles" do
+				expect(plane_787.roles).to eq(['pilot', 'co_pilot'])
+			end
+
+			it "#crew_requirement? knows if it has the right crew members to fly" do
+				expect(plane_787.crew_requirement?).to be true
+			end
+		end
+
+		it "#crew_requirement should be false since there's no crew" do
+			expect(plane_787.crew_requirement?).to be false
+		end
+
+
+	  it "#missing_crew_roles tells who is missing to make flight" do
+	  	expect(plane_787.missing_crew_roles).to eq({ pilot: 1, co_pilot: 1 })
+	  end
+	end
+
+	describe "passenger info" do
+	  it "should return a list of passengers with their seat assignment"
+	  it "should know if there are conflicting seating"
 	end
 
 	describe "ready to take off" do
@@ -55,27 +110,5 @@ describe Plane do
 	  	it "if it has approval from air traffic control"
 	  	it "if it has approval from HQ"
 	  end
-	end
-
-	describe "crew info" do
-		before(:each) do
-			tom = double("Crew", name: "Tom", role: "Pilot")
-			jane = double("Crew", name: "Jane", role: "Co-Pilot")
-			plane_787.crew << { name: tom.name, role: tom.role }
-			plane_787.crew << { name: jane.name, role: jane.role }
-		end
-	  it "should return a list of people with their roles" do
-			expect(plane_787.crew).to eq [{name: "Tom", role: "Pilot"},
-																		{name: "Jane", role: "Co-Pilot"}]
-	  end
-	  it "knows if it has the right crew members to fly" do
-			expect(plane_787.crew_requirement?).to be true
-	  end
-	  it "tells who is missing to make flight"
-	end
-
-	describe "passenger info" do
-	  it "should return a list of passengers with their seat assignment"
-	  it "should know if there are conflicting seating"
 	end
 end
